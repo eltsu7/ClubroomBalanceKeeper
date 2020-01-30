02351a0d13ab346fd90f5a54987e8f7dfccd12f7
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-01-30 12:15:41 +0200

    Database changes, id's from bigserial to uuid.

--- cbk.sql
+++ cbk.sql
@@ -1,27 +1,35 @@
 -- PostrgreSQL schema for Clubroom Balance Keeper database
 
-CREATE DATABASE ClubroomBalanceKeeper;
-
-CREATE USER cbk_user;
-
-GRANT ALL PRIVILEGES ON DATABASE ClubroomBalanceKeeper TO cbk_user;
-
-CONNECT ClubroomBalanceKeeper;
+-- UUID generator module
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS product (
-    name            VARCHAR(50)     NOT NULL PRIMARY KEY,
+    id              UUID            NOT NULL PRIMARY KEY,
+    name            VARCHAR(50)     NOT NULL,
     category        VARCHAR(50)     NOT NULL,
     price           INT             NOT NULL,
+    active          BOOLEAN         NOT NULL DEFAULT TRUE,
+
+    UNIQUE(id),
 );
 
 CREATE TABLE IF NOT EXISTS user (
-    id              BIGINT          NOT NULL PRIMARY KEY,
-    balance         INT             NOT NULL,
+    id              UUID            NOT NULL PRIMARY KEY,
+    balance         INT             NOT NULL DEFAULT 0 CHECK balance >= 0,
+    telegram_id     BIGINT,
+
+    UNIQUE(id),
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
-    id              BIGINT          NOT NULL PRIMARY KEY,
-    description     VARCHAR(255)    NOT NULL,
+    id              UUID            NOT NULL PRIMARY KEY,
     amount          INT             NOT NULL,
     date            DATE            NOT NULL,
+    product_id      UUID            NOT NULL,
+    user_id         UUID            NOT NULL,
+    description     VARCHAR(255),
+
+    FOREIGN KEY (product_id) REFERENCES Product (id),
+    FOREIGN KEY (user_id) REFERENCES User (id),
+    UNIQUE(id),
 );
