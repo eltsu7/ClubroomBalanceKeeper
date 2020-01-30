a820305ba9346313d6cbe403486d38822f13c709
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-01-30 14:35:35 +0200

    Create view for users transactions

--- cbk.sql
+++ cbk.sql
@@ -4,27 +4,32 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS product (
-    id              UUID            NOT NULL PRIMARY KEY,
+    id              UUID            NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     name            VARCHAR(50)     NOT NULL,
     category        VARCHAR(50)     NOT NULL,
     price           INT             NOT NULL,
-    active          BOOLEAN         NOT NULL DEFAULT "true"
+    active          BOOLEAN         NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE IF NOT EXISTS cbk_user (
-    id              UUID            NOT NULL PRIMARY KEY,
+    id              UUID            NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     balance         INT             NOT NULL DEFAULT 0 CHECK (balance >= 0),
     telegram_id     BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
-    id              UUID            NOT NULL PRIMARY KEY,
-    amount          INT             NOT NULL,
-    date            DATE            NOT NULL,
+    id              UUID            NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     product_id      UUID            NOT NULL,
-    user_id         UUID            NOT NULL,
+    cbk_user_id     UUID            NOT NULL,
+    date            DATE            NOT NULL,
     description     VARCHAR(255),
 
     FOREIGN KEY (product_id) REFERENCES product (id),
     FOREIGN KEY (user_id) REFERENCES cbk_user (id)
 );
+
+CREATE VIEW user_transactions AS
+    SELECT cbk_user.id, product.name, product.price
+    FROM transaction
+    INNER JOIN product ON transaction.product_id = product.id
+    INNER JOIN cbk_user ON transaction.cbk_user_id = cbk_user.id;
