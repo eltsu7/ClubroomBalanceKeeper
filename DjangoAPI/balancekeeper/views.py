6a56122682cdc72afb6ab35cda4094503b4890ba
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-02-04 13:19:21 +0200

    Add x times bought -counter to categorys product listing

--- DjangoAPI/balancekeeper/views.py
+++ DjangoAPI/balancekeeper/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Category, Product
+from .models import Category, Product, Transaction
 
 
 def category_summary(request):
     categories = list(Category.objects.values())
     
     # Count number of products and add it to category object
-    for catIndex in range(len(categories)):
-        cat = categories[catIndex]
+    for cat_i in range(len(categories)):
+        cat = categories[cat_i]
 
         try:
             productCount = len(Product.objects.filter(category=cat['id']))
         except ObjectDoesNotExist:
             productCount = 0
 
-        categories[catIndex]['productCount'] = productCount
+        categories[cat_i]['productCount'] = productCount
 
     return render(request, 'category_list.html', {'categories': categories})
 
@@ -26,9 +26,13 @@ def category_detail(request, id):
 
     try:
         category = Category.objects.get(id=id)
-        products = Product.objects.filter(category=category.id)
-        return render(request, 'category_detail.html', {'category': category, 'products': products})
+        products = list(Product.objects.filter(category=category.id).values())
     
     except ObjectDoesNotExist:
         # TODO: over the top 404-page
         return HttpResponse('<h4>Category not found</h4>')
+
+    for prod_i in range(len(products)):
+        products[prod_i]['times_bought'] = len(Transaction.objects.filter(product_id=products[prod_i]['id']))
+
+    return render(request, 'category_detail.html', {'category': category, 'products': products})
