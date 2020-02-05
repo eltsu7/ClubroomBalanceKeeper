b4adb09d78974514e0831a6d732ddf31ba7215f6
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-02-05 10:34:42 +0200

    Add post-function boilerplate for /product/

--- DjangoAPI/balancekeeper/views.py
+++ DjangoAPI/balancekeeper/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import parser_classes
 from rest_framework.parsers import JSONParser
 from .serializers import ProductSerializer
-from django.views.decorators.csrf import csrf_exempt
+import json
 
+from .keys import API_KEYS
 from .models import Category, Product, Transaction
 
 @csrf_exempt
+@parser_classes([JSONParser])
 def product_list(request):
     if request.method == 'GET':
         products = Product.objects.filter(active=True)
         serializer = ProductSerializer(products, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        if data['api_key'] and data['api_key'] in API_KEYS:
+            return HttpResponse('jee')
+        else:
+            return HttpResponse(':(')
+
+        
 
 @csrf_exempt
 def product_category_list(request, category_id):
@@ -23,8 +36,6 @@ def product_category_list(request, category_id):
 
 
 def stats(request):
-    # TODO rewrite
-
     products = list(Product.objects.filter(active=True))
     categories = list(Category.objects.values())
 
