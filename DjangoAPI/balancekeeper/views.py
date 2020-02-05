8c82963f01f6d125f696189092143533313bb075
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-02-05 11:07:08 +0200

    Implement logic and error handling to product/ post-method

--- DjangoAPI/balancekeeper/views.py
+++ DjangoAPI/balancekeeper/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden, HttpResponseBadRequest
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import parser_classes
@@ -20,10 +20,20 @@ def product_list(request):
 
     if request.method == 'POST':
         data = json.loads(request.body)
-        if data['api_key'] and data['api_key'] in API_KEYS:
-            return HttpResponse('jee')
-        else:
-            return HttpResponse(':(')
+
+        # Return status code 403 if api key isn't valid
+        if not (data['api_key'] and data['api_key'] in API_KEYS):
+            return HttpResponseForbidden('Invalid API key.')
+
+        # Return status code 400 if fields are missing
+        if not (all (field in data for field in ['name', 'category', 'price'])):
+            return HttpResponseBadRequest()
+
+        newProduct = Product(name=data['name'], category_id=data['category'], price=data['price'])
+        newProduct.save()
+
+        serializer = ProductSerializer(newProduct)
+        return JsonResponse(serializer.data, safe=False)
 
         
 
