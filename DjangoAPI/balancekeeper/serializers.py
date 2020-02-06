00a3cf2e6b595930f4c67b8d1199fe4f117e5908
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-02-06 08:36:12 +0200

    Add serializer for cbkuser

--- DjangoAPI/balancekeeper/serializers.py
+++ DjangoAPI/balancekeeper/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, CbkUser
 import uuid
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'category', 'price']
+
+class CbkUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CbkUser
+        fields = ['id', 'name', 'telegram_id']
