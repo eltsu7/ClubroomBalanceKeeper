c0d0c4793e0201566cf15ff6a1d743a991d9110d
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2020-02-16 18:02:04 +0200

    Turn off DEBUG-mode

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # ^ Moved to local_settings.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'eelih.fi'
